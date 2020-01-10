1bb3e76236f487d7f9368254933143866efd885a
MateusMello1995 <mellowmateus@outlook.com>
2020-01-10 10:26:19 -0300

    testando

--- app/src/main/java/com/example/mockup/telaSobre.java
+++ app/src/main/java/com/example/mockup/telaSobre.java
@@ -3,7 +3,9 @@ package com.example.mockup;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class telaSobre extends AppCompatActivity {
 
@@ -15,5 +17,10 @@ public class telaSobre extends AppCompatActivity {
             setSupportActionBar(myToolbar);
             getSupportActionBar().setDisplayShowTitleEnabled(false);
         }
+    public void proximaTela(View view){
+
+        Intent intent = new Intent(this, telaTermos.class);
+        startActivity(intent);
+    }
     }
 
