dfdc9bfcc71240c94155191893addaae989be4bc
MateusMello1995 <mellowmateus@outlook.com>
2020-01-14 09:51:14 -0300

    commit diligência final

---
+++ app/src/main/java/com/example/mockup/login.java
@@ -0,0 +1,25 @@
+package com.example.mockup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        }
+    public void proximaTela(View view){
+
+        Intent intent = new Intent(this, telaSobre.class);
+        startActivity(intent);
+    }
+        }
+
+
+
