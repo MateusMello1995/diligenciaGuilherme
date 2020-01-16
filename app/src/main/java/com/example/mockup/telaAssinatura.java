bef5adcec52c6fcc00ef211f8c4aed7e60f92269
MateusMello1995 <mellowmateus@outlook.com>
2020-01-16 10:13:46 -0300

    commit com menu e tela cadastro

--- app/src/main/java/com/example/mockup/telaAssinatura.java
+++ app/src/main/java/com/example/mockup/telaAssinatura.java
@@ -2,7 +2,9 @@ package com.example.mockup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class telaAssinatura extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class telaAssinatura extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_assinatura);
+    }
+    public void proximaTela(View view){
+
+        Intent intent = new Intent(this, telabranco.class);
+        startActivity(intent);
     }
 }
